7b952899cac8531b6025330e4852534cbaf0d27b
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-02-23 14:32:18 +0700

    Ubah semua

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # TOKEN = '4213543775:AsfjgfgdsdhfsdjhgjhfgsjgY'
 
 
-API_ID = ''
-API_HASH = ''
-TOKEN = ''
+API_ID = '27820192'
+API_HASH = '0b97b14b77c4f8671cfdc6d299ae06dc'
+TOKEN = '6257039911:AAFD2BACblZww98KpCsfap5auzYpcUY8vFA'
